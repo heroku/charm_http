73baaf1626cd3f0a6f9a134098fcd25d28213e08
Blake Gentry <blakesgentry@gmail.com>
2013-06-14 11:51:28 -0700

    rename apt package git -> git-core

--- lib/charm_http/setup.rb
+++ lib/charm_http/setup.rb
@@ -28,7 +28,7 @@ class CharmHttp
       end
 
       [
-      "yes | sudo apt-get install make gcc git libevent-dev",
+      "yes | sudo apt-get install make gcc git-core libevent-dev",
       "git clone https://github.com/archaelus/hummingbird.git || (cd hummingbird && git pull)",
       "cd hummingbird && make hstress"
       ].each do |command|
