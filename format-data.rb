ca05e7dd5067652513a56872b31511ffe3fb92cb
Blake Gentry <blakesgentry@gmail.com>
2013-06-18 15:15:59 -0700

    remove old hard-coded result

--- format-data.rb
+++ format-data.rb
@@ -1,37 +1,5 @@
 #!/usr/bin/env ruby
 
-res = {
-  "canary-go-shin.herokuapp.com"=>{
-    20=>{
-      100=>{
-        1=> {
-          0=> {
-            "hz"=>4953,
-             "time"=>3598,
-             "conn_total"=>893624,
-             "conn_successes"=>891847,
-             "conn_errors"=>0,
-             "conn_timeouts"=>1777,
-             "conn_closes"=>37400,
-             "http_successes"=>891847,
-             "http_errors"=>0,
-             "<1"=>1,
-             "<5"=>17386,
-             "<10"=>60100,
-             "<15"=>140805,
-             "<20"=>498344,
-             "<30"=>160715,
-             "<40"=>11640,
-             "<50"=>1792,
-             "<70"=>498,
-             "<100"=>381,
-             "<150"=>116,
-             "<300"=>48,
-             "<500"=>15,
-             "<1000"=>5,
-             ">=1000"=>1
-}}}}}}
-
 filename = ARGV[0]
 if filename.nil? || filename == ""
   puts "please specify a filename for the data file to read"
