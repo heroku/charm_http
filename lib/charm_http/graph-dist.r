28ebe5fcc000a2507a7ba7c134a05c1cc61ed4ae
Blake Gentry <blakesgentry@gmail.com>
2013-06-21 10:29:45 -0700

    first working version of graphing response distribution charts

---
+++ lib/charm_http/graph-dist.r
@@ -0,0 +1,107 @@
+# load qcc package
+library(ggplot2)
+require(scales)
+
+# Parse args
+args <- commandArgs(TRUE)
+if(length(args) != 1 || is.na(args[1])) {
+  "ERROR: Missing output filename"
+  q(status=1)
+} else {
+  outputfile = args[1]
+}
+
+# Load data
+data = read.table('tmp/data-dist.ssv', header=T, sep=" ")
+
+# Convert count to fraction of total count
+total_counts <- sum(data$count)
+data$percent <- data$count / total_counts
+data$buckets <- with(data, reorder(buckets, order))
+
+
+# nservices = ncol(data) - 1
+# nrows = nrow(data)
+
+# Sort column headers
+# data = data[,c(c(1), order(names(data[2:ncol(data)]))+c(1))]
+# headers = names(data)
+
+# Get X range
+# xrange = range(data[1])
+
+# Get Y range
+# ymax = 0
+# for (i in 1:nservices) {
+#   curmax = max(data[i + 1])
+#   if (curmax > ymax) {
+#     ymax = curmax
+#   }
+# }
+# yrange = c(0,ymax)
+
+# Set up plot
+png(outputfile, width=8, height=6, units = 'in', res=150)
+# colors = rainbow(nservices)
+
+# Add bars
+bar_chart <- ggplot(data, aes(x=buckets, y=percent), colour="blue") +
+  geom_bar() +
+  xlab("Response Time (ms)") +
+  ylab("Percent") +
+  scale_y_continuous(labels = percent_format(), limits=c(0,1))
+# bar_chart
+
+fortify_pareto_data <- function(data, xvar, yvar, sort = TRUE)
+{
+  for(v in c(xvar, yvar))
+  {
+    if(!(v %in% colnames(data)))
+    {
+      stop(sQuote(v), " is not a column of the dataset")
+    }
+  }
+
+  if(sort) {
+    o <- order(data[, yvar], decreasing = TRUE)
+    data <- data[o, ]
+    data[, xvar] <- factor(data[, xvar], levels = data[, xvar])
+  }
+
+  data[, yvar] <- as.numeric(data[, yvar])
+  data$.cumulative.y <- cumsum(data[, yvar])
+
+  data$.numeric.x <- as.numeric(data[, xvar])
+  data
+}
+
+fortified_data <- fortify_pareto_data(data, "buckets", "percent", sort=FALSE)
+
+pareto_plot <- bar_chart %+% fortified_data +
+    geom_line(aes(.numeric.x, .cumulative.y)) +
+    ylab("Cumulative Percentage") +
+    scale_y_continuous(labels = percent_format(), limits=c(0,1))
+pareto_plot
+
+
+
+
+# Add lines
+# if (nrows < 2) {
+#   data.mx = as.matrix(data[2:ncol(data)])
+#   par(las=3, mar=c(5,5,3,1))
+#   barplot(data.mx, yaxp=c(0,ymax,4), beside=T, col=colors, xlab=paste(data[1,1], "dynos"), ylab="Responses per second", border=T, names.arg=rep("", nservices))
+# } else {
+#   for (i in 1:nservices) {
+#     if (nrows < 4) {
+#       lines(data[[1]], data[[i + 1]], type="l", lwd=3, lty=1, col=colors[i])
+#     } else {
+#       data.spl = smooth.spline(data[[1]], data[[i + 1]], spar=0.3)
+#       lines(predict(data.spl, seq(xrange[1], xrange[2], by=0.1)), type="l", lwd=3, lty=1, col=colors[i])
+#     }
+#   }
+# }
+
+# Add title and legend
+# title(headers[1])
+# legend(xrange[1], yrange[2] * 0.95, headers[2:ncol(data)], cex=0.8, col=colors, bg='transparent', fill=colors, border=colors)
--- lib/charm_http/graph.rb
+++ lib/charm_http/graph.rb
@@ -3,8 +3,9 @@ class CharmHttp
   class Graph
     def self.graph(filename)
       graph = Graph.new(filename)
-      graph.make_data
-      graph.make_chart
+      graph.make_req_data
+      graph.make_req_chart
+      graph.make_dist_charts
     end
 
     def initialize(filename)
@@ -12,7 +13,7 @@ class CharmHttp
       @data = eval(File.read(@filename))
       @headers = @data.keys
       @workers = @data[@headers.first].keys.first
-      @x = @headers.map do |header|
+      @concurrencies = @headers.map do |header|
         begin
           @data[header][@workers].keys
         rescue NoMethodError
@@ -22,15 +23,15 @@ class CharmHttp
       end.flatten.uniq.sort
     end
 
-    def make_data
+    def make_req_data
       Dir.mkdir("tmp") rescue nil
       File.open("tmp/data-reqs.ssv", "w") do |ssv|
-        ssv.puts "#{@filename} #{@headers.join(' ')}"
-        @x.each do |x|
-          ssv.write "#{@data[@headers.first][@workers][x].keys.first} "
+        ssv.puts "#{@filename}.reqs #{@headers.join(' ')}"
+        @concurrencies.each do |c|
+          ssv.write "#{@data[@headers.first][@workers][c].keys.first} "
 
           data = @headers.map do |header|
-            hash = @data[header][@workers][x]
+            hash = @data[header][@workers][c]
             hash = hash[hash.keys.first]
 
             max = 0
@@ -49,8 +50,44 @@ class CharmHttp
       puts File.read("tmp/data-reqs.ssv")
     end
 
-    def make_chart
+    def make_dist_data(concurrency)
+      num_dynos = @data[@headers.first][@workers][concurrency].keys.first
+      Dir.mkdir("tmp") rescue nil
+      File.open("tmp/data-dist.ssv", "w") do |ssv|
+        ssv.puts "order buckets count"
+
+        all_runs = @data[@headers.first][@workers][concurrency][num_dynos]
+        hash1 = all_runs[all_runs.keys.first]
+        buckets = hash1.keys.select{|k| k =~ /^(?:<|>=)\d+/ }
+
+        all_values = buckets.inject({}) {|h, k| h[k] = []; h }
+        all_runs.each do |run_number, data|
+          buckets.each{|k| all_values[k] << data[k] }
+        end
+
+        buckets.each_with_index do |k, i|
+          ssv.puts "#{i} #{k} #{avg(all_values[k]).round}"
+        end
+
+        puts File.read("tmp/data-dist.ssv")
+      end
+    end
+
+    def make_req_chart
       system("R --vanilla < #{LIB}/charm_http/graph-reqs.r")
+    end
+
+    def make_dist_charts
+      @concurrencies.each do |c|
+        make_dist_data(c)
+        num_dynos = @data[@headers.first][@workers][c].keys.first
+        output = "#{@filename}.dist.#{num_dynos.to_s.rjust(2, '0')}.dynos.c#{c}.png"
+        system("R --vanilla < #{LIB}/charm_http/graph-dist.r --args #{output}")
+      end
+    end
+
+    def avg(arr = [])
+      arr.inject(0.0) { |sum, el| sum + el } / arr.size
     end
   end
 end
